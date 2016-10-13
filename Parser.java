d10f8c5363de19663770fcde9a87013cea3d8b31
nick <red@localhost.localdomain>
2016-10-13 12:12:26 -0600

    initial file and readme additions

---
+++ Parser.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Parser
+{
+    public static void main( String [] args )
+    {
+	System.out.print( "Enter Expression: " );
+	Scanner sc = new Scanner( System.in );
+	String expression = sc.next();
+	System.out.println( " expression: " + expression );
+    }
+}
