1fbfe4cd3bd7f5d82d9ae241fab9ec8a9baebf02
nick <red@localhost.localdomain>
2016-10-17 10:57:53 -0600

    string parsing started and cmd line arguments finished

--- Parser.java
+++ Parser.java
@@ -2,11 +2,60 @@ import java.util.*;
 
 public class Parser
 {
+    public static boolean tokenFlag = false;
+
     public static void main( String [] args )
     {
-	System.out.print( "Enter Expression: " );
-	Scanner sc = new Scanner( System.in );
-	String expression = sc.next();
-	System.out.println( " expression: " + expression );
+	parseCMD( args );
+	readInput();
+    }
+ 
+    public static void parseCMD(String [] args)
+    {
+	for( String arg : args )
+ 	{
+	    if( arg.equals("-t") )
+	    {
+		tokenFlag = true;
+            }
+	}
+    }    
+
+    public static void readInput()
+    {
+	//exit condition
+	boolean exit = false;
+	while( exit == false)
+	{
+	    System.out.print( "Enter Expression: " );
+	    Scanner sc = new Scanner( System.in );
+	    String expression = sc.nextLine();
+
+	    // check for empty string
+	    if( expression.isEmpty() )
+	    {
+		System.out.println( "(end of input)" );
+		return; //exit the program
+            }
+	    else
+            { 
+	        System.out.println( " expression: " + expression );
+		if( tokenFlag == true)
+		{
+		    splitExpression( expression );
+		}
+            }
+	}
+    }
+
+    public static void splitExpression(String expression)
+    {
+	String delims = "+-()*%";
+
+	StringTokenizer st = new StringTokenizer( expression, delims );
+	while( st.hasMoreElements())
+        {
+	     System.out.println("Token: " + st.nextElement());
+        }
     }
 }
